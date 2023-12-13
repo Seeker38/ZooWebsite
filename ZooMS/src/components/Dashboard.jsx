d295624f22f382cc12db599b4d9bfb86ba8114c9
Hector <truongminhhieu888@gmail.com>
2023-12-14 00:15:03 +0700

    mod dashboard

--- ZooMS/src/components/Dashboard.jsx
+++ ZooMS/src/components/Dashboard.jsx
@@ -9,25 +9,40 @@ function DashBoard() {
         <div className='col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark'>
           <div className='d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100'>
             <Link to= "/dashboard" className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none">
-              <span className="fs-5 fw-bolder d-none d-sm-inline">
+              <span className="fs-5 fw-bolder d-none d-sm-inline ">
                   Zoolander
                 </span>
             </Link>
             <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
               <li className="w-100">
-                <Link to = "/dashboard" className="nav-link px-0 align-middle text-white"> Dashboard</Link>
+                <Link to = "/dashboard" className="nav-link px-0 align-middle text-white"> 
+                  <i className="fs-4 bi bi-menu-button-wide ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Dashboard</span>
+                </Link>
               </li>
-              <li>
-                <Link to = "/dashboard"> Manage employee</Link>
+              <li className="w-100">
+                <Link to = "/dashboard" className="nav-link px-0 align-middle text-white"> 
+                  <i className="fs-4 bi-people ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Manage Employee</span>
+                </Link>
+              </li >
+              <li className="w-100">
+                <Link to = "/dashboard" className="nav-link px-0 align-middle text-white"> 
+                  <i className="fs-4 bi bi-tags ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Category</span>
+                </Link>
               </li>
-              <li>
-                <Link to = "/dashboard"> Category</Link>
+              <li className="w-100">
+                <Link to = "/dashboard" className="nav-link px-0 align-middle text-white"> 
+                  <i className="fs-4 bi bi-person-square ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Profile</span>
+                </Link>
               </li>
-              <li>
-                <Link to = "/dashboard"> Profile</Link>
-              </li>
-              <li>
-                <Link to = "/dashboard"> SignOut</Link>
+              <li className="w-100">
+                <Link to = "/dashboard" className="nav-link px-0 align-middle text-white"> 
+                  <i className="fs-4 bi bi-door-open ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Logout</span>
+                </Link>
               </li>
             </ul>
           </div>
