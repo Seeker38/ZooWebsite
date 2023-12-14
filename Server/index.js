52cd559620be42cafb347bee4cd7dc33b2812621
Hector <truongminhhieu888@gmail.com>
2023-12-14 23:01:20 +0700

    add employee

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,17 @@
 import express from "express";
-import cors from "cors";
-import {adminRouter} from "./Routes/AdminRoute.js";
+import cors from 'cors'
+import { adminRouter } from "./Routes/AdminRoute.js";
 
 const app = express();
 app.use(cors({    
     origin: ["http://localhost:5173"],
-    methods: ["GET", "PUT", "POST"],
+    methods: ["GET", "PUT", "POST" , "DELETE"],
     credentials: true
 }));
+
 app.use(express.json());
 app.use("/auth", adminRouter);  
+app.use(express.static("Public"));
 
 app.listen(3000, () => {
     console.log("Server is running")
--- ZooMS/src/App.jsx
+++ ZooMS/src/App.jsx
@@ -9,6 +9,7 @@ import Employee from './components/Employee'
 import Category from './components/Category'
 import Profile from './components/Profile'
 import AddCategory from './components/AddCategory'
+import AddEmployee from './components/AddEmployee'
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
         <Route path='/dashboard/category' element={<Category />}></Route>
         <Route path='/dashboard/profile' element={<Profile />}></Route>
         <Route path='/dashboard/add_category' element={<AddCategory />}></Route>
+        <Route path='/dashboard/add_employee' element={<AddEmployee />}></Route>
       </Route>
     </Routes>
   </BrowserRouter>
