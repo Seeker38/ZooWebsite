4f5342eebf7b9f5cd7437381b6eab3cd2bb26187
Hector <truongminhhieu888@gmail.com>
2023-12-15 18:05:28 +0700

    update EditEmployee and Home

--- Server/Routes/AdminRoute.js
+++ Server/Routes/AdminRoute.js
@@ -114,6 +114,47 @@ router.put('/edit_employee/:id', (req, res) => {
   })
 })
 
+router.delete('/delete_employee/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = "delete from employee where id = ?"
+  con.query(sql,[id], (err, result) => {
+      if(err) return res.json({Status: false, Error: "Query Error"})
+      return res.json({Status: true, Result: result})
+  })
+})
+
+router.get("/admin_count", (req, res) => {
+  const sql = "select count(id) as admin from admin";
+  con.query(sql, (err, result) => {
+    if (err) return res.json({Status: false, Error: "Query Error"})
+    return res.json({Status: true, Result: result})
+  })
+})
+
+router.get('/employee_count', (req, res) => {
+  const sql = "select count(id) as employee from employee";
+  con.query(sql, (err, result) => {
+      if(err) return res.json({Status: false, Error: "Query Error"})
+      return res.json({Status: true, Result: result})
+  })
+})
+
+router.get('/salary_count', (req, res) => {
+  const sql = "select sum(salary) as TotalSalary from employee";
+  con.query(sql, (err, result) => {
+      if(err) return res.json({Status: false, Error: "Query Error"})
+      return res.json({Status: true, Result: result})
+  })
+})
+
+router.get('/admin_records', (req, res) => {
+  const sql = "select * from admin"
+  con.query(sql, (err, result) => {
+      if(err) return res.json({Status: false, Error: "Query Error"})
+      return res.json({Status: true, Result: result})
+  })
+})
+
 export {router as adminRouter};
 
 // import express from 'express';
